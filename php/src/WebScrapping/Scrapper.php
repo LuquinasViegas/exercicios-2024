cd008649848fda7b11df2313b7d459a8af368f9e
Lucas Viegas <lucas.viegas3005@gmail.com>
2024-05-09 08:59:03 -0300

    scrapping de dados dos autores

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -35,8 +35,32 @@ class Scrapper {
       $paperArray['title'] = $title[0]->nodeValue;
       $paperArray['type'] = $type[0]->nodeValue;
 
-  
 
+      // Retorna a div da lista de autores 'div.authors' do Paper
+      $authorsDiv = $xpath->query(".//following::div[contains(@class,'authors')]", $paper);
+    
+      // Para cada 'span' em 'div.authors'
+      $authors = $xpath->query(".//span", $authorsDiv[0]);
+
+      // Cria array de objetos de autores
+      $objAuthorsArray = array();
+      foreach($authors as $author) {
+        // Cria array individual para cada autor
+        $authorArray = array();
+
+        // Condição necessária pois no ID 137457 após RAFAEL ALVES DE ANDRADE existe um campo vazio
+        if($author->nodeValue !="") {
+        // Retorna o valor absoluto do nó
+        $authorName = str_replace(";","", $author->nodeValue);
+        // Retorna o valor do atributo 'title'
+        $authorInstitution = $author->attributes["title"]->nodeValue;
+        $authorArray['name'] = $authorName;
+        $authorArray['institution'] = $authorInstitution;
+        // Inseree no array individual nome e intituição do autor
+        array_push($objAuthorsArray, new Person($authorName, $authorInstitution));
+        }
+
+      }
 
     }
     return [
